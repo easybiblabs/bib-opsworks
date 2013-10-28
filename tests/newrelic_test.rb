f3124072379b5a029c30a7f408c38af9931a9513
fh <fh-github@fholzhauer.de>
2013-10-28 19:06:09 +0100

    added test for the empty deploying_user workaround

--- tests/newrelic_test.rb
+++ tests/newrelic_test.rb
@@ -14,4 +14,14 @@ class NewrelicTest < Minitest::Test
     result = newrelic.prepare_publishing_data(app_name, deploy_data)
     assert_equal("deployment[app_name]=unittest&deployment[user]=gemtest&=deployment[revision]=rev1", result)
   end
+  
+  def test_publish_deployment_nouser
+    newrelic = Bib::Opsworks::Newrelic.new()
+    
+    deploy_data = { 'scm' => { 'revision' => 'rev1' } }
+    app_name = 'unittest'
+    
+    result = newrelic.prepare_publishing_data(app_name, deploy_data)
+    assert_equal("deployment[app_name]=unittest&deployment[user]=opsworks&=deployment[revision]=rev1", result)
+  end
 end
