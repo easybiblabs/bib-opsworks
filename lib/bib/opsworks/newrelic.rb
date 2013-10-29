84c1ea8ff3993df62d3640ab3d2ca449cc2bdfe7
Robin Mehner <robin@coding-robin.de>
2013-10-29 16:34:06 +0100

    Dead to whitespaces!

--- lib/bib/opsworks/newrelic.rb
+++ lib/bib/opsworks/newrelic.rb
@@ -4,9 +4,9 @@ module Bib
     class Newrelic
 
       def publish_deployment(app_name, deploy_data, newrelic_api_key)
-        
+
         newrelic_body = prepare_publishing_data(app_name, deploy_data)
-        
+
         newrelic_url = "https://rpm.newrelic.com/deployments.xml"
         url = URI.parse(newrelic_url)
         request = Net::HTTP::Post.new(url.request_uri)
@@ -15,16 +15,16 @@ module Bib
         resp = Net::HTTP.new(url.host, url.port).start do |http|
             http.request(request)
         end
-        
+
         case resp
         when Net::HTTPOK
           return true
         else
           return false
         end
-        
+
       end
-      
+
       def prepare_publishing_data(app_name, deploy_data)
         scm_revision = deploy_data["scm"]["revision"]
 
@@ -33,15 +33,15 @@ module Bib
         else
           deployment_user = deploy_data["deploying_user"].split('/')[1]
         end
-        
+
         newrelic_appid_body = "deployment[app_name]=#{app_name}"
         newrelic_user_body = "deployment[user]=#{deployment_user}"
         newrelic_revision_body = "deployment[revision]=#{scm_revision}"
-        
+
         newrelic_body = "#{newrelic_appid_body}&#{newrelic_user_body}&=#{newrelic_revision_body}"
-        
+
         return newrelic_body
-        
+
       end
     end
   end
--- tests/newrelic_test.rb
+++ tests/newrelic_test.rb
@@ -4,23 +4,23 @@ require 'minitest/autorun'
 require 'bib/opsworks/newrelic'
 
 class NewrelicTest < Minitest::Test
-  
+
   def test_publish_deployment
     newrelic = Bib::Opsworks::Newrelic.new()
-    
+
     deploy_data = { 'deploying_user' => 'arn:aws:iam::123456:user/gemtest', 'scm' => { 'revision' => 'rev1' } }
     app_name = 'unittest'
-    
+
     result = newrelic.prepare_publishing_data(app_name, deploy_data)
     assert_equal("deployment[app_name]=unittest&deployment[user]=gemtest&=deployment[revision]=rev1", result)
   end
-  
+
   def test_publish_deployment_nouser
     newrelic = Bib::Opsworks::Newrelic.new()
-    
+
     deploy_data = { 'scm' => { 'revision' => 'rev1' } }
     app_name = 'unittest'
-    
+
     result = newrelic.prepare_publishing_data(app_name, deploy_data)
     assert_equal("deployment[app_name]=unittest&deployment[user]=opsworks&=deployment[revision]=rev1", result)
   end
