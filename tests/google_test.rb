175fc460950b10f7191b1191134b521f7e053f49
Florian Holzhauer <fh-github@fholzhauer.de>
2015-03-30 21:17:27 +0200

    Adapting google analytics code to rubocop standards

--- tests/google_test.rb
+++ tests/google_test.rb
@@ -4,7 +4,6 @@ require 'minitest/autorun'
 require 'bib/opsworks/google'
 
 class GoogleTest < Minitest::Test
-
   def test_publish_deployment_user
     google = Bib::Opsworks::Google.new
 
@@ -12,7 +11,11 @@ class GoogleTest < Minitest::Test
     app_name = 'unittest'
 
     result = google.prepare_publishing_data(app_name, deploy_data)
-    expected = {"deployment[app_name]"=>"unittest", "deployment[user]"=>"gemtest", "deployment[revision]"=>"rev1"}
+    expected = {
+      'deployment[app_name]' => 'unittest',
+      'deployment[user]' => 'gemtest',
+      'deployment[revision]' => 'rev1'
+    }
     assert_equal(expected, result)
   end
 
@@ -23,12 +26,16 @@ class GoogleTest < Minitest::Test
     app_name = 'unittest'
 
     result = google.prepare_publishing_data(app_name, deploy_data)
-    expected = {"deployment[app_name]"=>"unittest", "deployment[user]"=>"opsworks", "deployment[revision]"=>"rev1"}
+    expected = {
+      'deployment[app_name]' => 'unittest',
+      'deployment[user]' => 'opsworks',
+      'deployment[revision]' => 'rev1'
+    }
     assert_equal(expected, result)
   end
 
   def test_publish_deployment
-   google = Bib::Opsworks::Google.new
+    google = Bib::Opsworks::Google.new
 
     deploy_data = { 'deploying_user' => 'arn:aws:iam::123456:user/gemtest', 'scm' => { 'revision' => 'rev1' } }
     app_name = 'unittest'
